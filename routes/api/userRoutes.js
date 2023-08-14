73693da936735388633bdb14a671888293a6f1f1
kelso1020 <kelseyjessica1710@gmail.com>
2023-08-14 13:03:46 -0400

    added api routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  deleteUser,
+  updateUser,
+  createFriend,
+  deleteFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getUser).delete(deleteUser).put(updateUser);
+
+// /api/users/:userId/friends/friendId
+router
+    .route("/;userId/friends/:friendId")
+    .post(createFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
