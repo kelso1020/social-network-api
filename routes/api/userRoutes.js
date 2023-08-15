e8529f4814f614ebf035439c9cf98de2e232587e
kelso1020 <kelseyjessica1710@gmail.com>
2023-08-14 21:43:15 -0400

    debug

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -17,7 +17,7 @@ router.route("/:userId").get(getUser).delete(deleteUser).put(updateUser);
 
 // /api/users/:userId/friends/friendId
 router
-    .route("/;userId/friends/:friendId")
+    .route("/:userId/friends/:friendId")
     .post(createFriend)
     .delete(deleteFriend);
 
